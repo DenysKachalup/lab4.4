16d9d7ee23de15cd2acab66ceac38cee22f77282
Denys-Maksym Kachalup <Denys-Maksym.Kachalup.ITIS.2020@edu.lpnu.ua>
2021-04-05 17:09:43 +0300

    lab

---
+++ lab4.4/Ball.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"Body.h"
+
+class Ball: public Body
+{
+public:
+	Ball();
+	Ball(int a);
+	virtual double Plos();
+	virtual double Obj();
+	virtual void Read();
+	virtual void Print();
+};
+
